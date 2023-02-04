1203ae44640cd559c506709a2fba47f37d9d6633
kamran rahmani <rahmanikamran95@gmail.com>
2023-02-03 23:03:56 -0500

    add env vars

--- database/config.js
+++ database/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  host: DATABASE_URL,
-  db: DATABASE_NAME,
-  username: DATABASE_USERNAME,
-  password: DATABASE_PASSWORD,
+  host: process.env.DATABASE_URL,
+  db: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
   dialect: "mysql",
 
   pool: {
