8d19a17898984e2230816f4372c61d9ee0889da5
kamran rahmani <rahmanikamran95@gmail.com>
2023-02-05 12:00:13 -0500

    database fix

--- database/config.js
+++ database/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   host: process.env.DATABASE_URL,
   db: process.env.DATABASE_NAME,
-  username: DATABASE_USERNAME,
-  password: DATABASE_PASSWORD,
+  username: process.env.DATABASE_USERNAME,
+  password:process.env.DATABASE_PASSWORD,
   dialect: "mysql"
 };
